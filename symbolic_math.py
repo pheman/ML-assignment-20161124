202ab88434738c946e424e34a2f795982426c179
pheman <rbzhang100@gmail.com>
2017-11-23 17:39:48 +0800

    Update symbolic_math.py

--- symbolic_math.py
+++ symbolic_math.py
@@ -16,3 +16,25 @@ Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 pd.set_option( 'mode.chained_assignment', 'raise')
 zhfont = FontProperties()
 zhfont.set_family('SimHei')
+
+def sumHub(dbcbudget, signratio, key, term):
+    saleSum = 0.0
+    for dbc in signratio.index:
+        try:
+            #print(dbc)
+            saleSum = saleSum + dbcbudget.loc[dbc].loc[term,key] * signratio[dbc]
+            #print(saleSum)
+        except KeyError:
+            print(dbc,'not in dbcbudget from sumHub')        
+    return saleSum
+
+pd.set_option('mode.chained_assignment','raise')
+zhfont = FontProperties()
+zhfont.set_family('SimHei')
+
+def check_index(df1, df2):
+    df1set = set(df1.index)
+    df2set = set(df2.index)
+    print('length =', len(df1), ' and ',len(df2))
+    print('only in left ', df1set - df2set)
+    print('only in right', df2set - df1set)
